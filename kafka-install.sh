74349d7783bfa7e5d8e702e3d6792a77cdacdaa2
Nagaraju Kusa <50620042+nagarajukusa24@users.noreply.github.com>
2024-02-27 18:01:03 +0530

    Create kafka-install.sh

---
+++ kafka-install.sh
@@ -0,0 +1,6 @@
+cd /opt
+curl -L -O https://downloads.apache.org/kafka/3.6.1/kafka_2.12-3.6.1.tgz
+tar -xf kafka_2.12-3.6.1.tgz
+mv kafka_2.12-3.6.1 kafka
+cd /opt/kafka
+for i in `ls *.sh` ; do ln -s /opt/kafka/bin/$i /bin/$i;done
