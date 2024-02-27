772c664b0fe49b002ae4d1138921ff4d3609394a
Nagaraju Kusa <50620042+nagarajukusa24@users.noreply.github.com>
2024-02-27 18:01:14 +0530

    Update kafka-install.sh

--- kafka-install.sh
+++ kafka-install.sh
@@ -2,5 +2,5 @@ cd /opt
 curl -L -O https://downloads.apache.org/kafka/3.6.1/kafka_2.12-3.6.1.tgz
 tar -xf kafka_2.12-3.6.1.tgz
 mv kafka_2.12-3.6.1 kafka
-cd /opt/kafka
+cd /opt/kafka/bin
 for i in `ls *.sh` ; do ln -s /opt/kafka/bin/$i /bin/$i;done
